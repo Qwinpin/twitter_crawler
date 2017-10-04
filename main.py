760526abcbfcb32747914ad302b4a1b4fd4741ef
Unknown <qwinpin@gmail.com>
2017-10-04 18:05:59 +0400

    deleted parameters class added examples

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from parameters import Parameters
 import off_api as oa
 import bypass_api as ba
+from setting import set_parameters
+
+#examples
+settings = set_parameters(screen_name = 'blessmepadre', maxTweets = 3)
+
+#off api
 #api = oa.login()
-para = Parameters().setUsername('blessmepadre').setSince('2017-09-01').setUntil('2017-09-03').setMaxTweets(30000)
-#ba.get_tweets_by_data(para)
-#a, b = oa.get_followers(api, para)
-#print(b)
-#c, d = oa.get_tweets_3200(api, para)
-#print(d)
-twe = ba.getTweets(para)
-print(twe[0].text)
-print(len(twe))
+#data_1, err = oa.get_tweets_3200(api, settings)
+#print (data_1[0].text.encode('utf-8'))
+#print(err)
+
+#nonoff api
+data_2, err = ba.get_tweets(settings)
+print(data_2[0].text)
+print(err)
