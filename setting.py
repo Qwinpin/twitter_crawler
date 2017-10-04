9de734c968a691be2651cc04e2cb98029f5f2082
Unknown <qwinpin@gmail.com>
2017-10-04 18:07:42 +0400

    New setting system

---
+++ setting.py
@@ -0,0 +1,19 @@
+def set_parameters(screen_name = None, maxTweets = 1, since = None, until = None, querySearch = None, 
+    topTweets = False, near = None, within = None):
+    parameters_url = (
+        (' from:', screen_name),
+        (' ', querySearch),
+        ('&near:', near),
+        (' within:', within),
+        (' since:', since),
+        (' until:', until),
+        ('&src=typd&max_position=', '')
+    )
+
+    parameters_api = {
+        'screen_name': screen_name, 
+        'maxTweets': maxTweets,
+        'topTweets': topTweets
+    }
+
+    return parameters_api, parameters_url
