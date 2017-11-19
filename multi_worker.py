6fd462ec4ab99c17fd2423183482e42b77ed0cf0
FormMe <formme1995@yandex.ru>
2017-11-20 00:27:34 +0300

    first step to multiprocess crawling

--- multi_worker.py
+++ multi_worker.py
@@ -1,21 +1,31 @@
-import bypass_api
-import setting
+import bypass_api as ba
+from setting import set_parameters, set_save_parameters
 import os
 import time
 from multiprocessing import Pool, Queue
 
 def main_worker(queue):
-    print(os.getpid()," working")
+    pid = os.getpid()
+    print(pid,"working")
     while True:
-        item = queue.get(True)
-        print(os.getpid(), "got", item)
-        time.sleep(1) # simulate a "long" operation
+        #TODO не забыть поставить timeout или отправлять в очередь пустую таску
+        task = queue.get(True)
+        query = task[0]
+        saving_params = task[1]
+        data, err, cook = ba.parse(query)
+        ba.to_csv(data, 'file_' + str(pid)+ '_', saving_params)
+        print(os.getpid(), "crawled", len(data), "tweets")
 
 def main():
     queue = Queue()
     pool = Pool(4, main_worker, (queue,))
-    for i in range(5):
-        queue.put("hello")
+    save_param = set_save_parameters()
+    for i in range(10,18):
+        task = set_parameters(maxTweets = 300,
+                              since=('2017-01-' + str(i)),
+                              until=('2017-01-' + str(i+1)),
+                              screen_name='xbox')
+        queue.put((task, save_param))
 
     pool.close()
     pool.join()
