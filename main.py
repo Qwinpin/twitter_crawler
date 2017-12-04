8514c8e7e7aef15737cb6875f543299952d52749
Unknown <qwinpin@gmail.com>
2017-12-05 02:26:21 +0400

    Refact

--- main.py
+++ main.py
@@ -3,32 +3,29 @@ import bypass_api as ba
 from setting import set_parameters, set_save_parameters
 #examples
 
-#off api
-#api = oa.login()
-#data_1, err = oa.get_tweets_3200(api, settings)
-#print (data_1[0].text.encode('utf-8'))
-#print(err)
-
-#nonoff api
-#in net implementation - class of workers, threads
 def worker():
-    #Set i - use when request sender is failed, err - use for debag, cook - if we failed in some iter we start it bagen from breakpoint
+    #Set i - use when request sender is failed, err - use for debag, 
+    #cook - if we failed in some iter we start it bagen from breakpoint
     #data - data at all
     i = True
     err = 'err_request'
     cook = None
     data = None
-    while ((err == 'err_request') and (i)):
+    while err == 'err_request' and i:
         print('start')
-        settings = set_parameters(maxTweets = 42, since = '2017-01-20', querySearch = 'cat', cookies = cook)
+        settings = set_parameters(since='2016-10-12', until='2016-12-14', querySearch='Путин')
         data, err, cook = ba.parse(settings)
         i = False
     return data, err
 
-data_2, err = worker()
-if data_2 is None:
-    print(err)
-else:
-    print(data_2[0].text)
-    ba.to_csv(data_2, 'tweets.csv', set_save_parameters())
-    print(err)
+def main():
+    data_2, err = worker()
+    if data_2 is None:
+        print(err)
+    else:
+        print(data_2[0].text)
+        ba.to_csv(data_2, 'tweets.csv', set_save_parameters())
+        print(err)
+
+if __name__ == '__main__':
+    main()
