60b2d0b2b2df0daaf3cf76803f245c77abfc0680
FormMe <formme1995@yandex.ru>
2017-12-05 00:13:48 +0300

    bypass tasks sending recieving

--- producer.py
+++ producer.py
@@ -1,23 +1,31 @@
 import pika
 import sys
 import json
+from setting import set_parameters, set_save_parameters
 import numpy as np
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host='localhost'))
+    host='localhost'))
 channel = connection.channel()
 
 channel.queue_declare(queue='task_queue', durable=True)
 
-message = {
-    'name': 'Ilya',
-    'age' : np.random.randint(60)
-}
-channel.basic_publish(exchange='',
-                      routing_key='task_queue',
-                      body=json.dumps(message),
-                      properties=pika.BasicProperties(
-                         delivery_mode = 2, # make message persistent
-                      ))
-print(" [x] Sent %r" % (message,))
+for i in range(10, 30):
+    message = {
+        'query_param': set_parameters(maxTweets=300,
+                             since=('2017-01-' + str(i)),
+                             until=('2017-01-' + str(i + 1)),
+                             screen_name='xbox'),
+
+        'save_param': set_save_parameters()
+    }
+
+    channel.basic_publish(exchange='',
+                          routing_key='task_queue',
+                          body=json.dumps(message),
+                          properties=pika.BasicProperties(
+                              delivery_mode=2,  # make message persistent
+                          ))
+    print(" [x] Sent %r" % (message,))
+
 connection.close()
