123315d89393ed372eb40eda1b563575a1616ef7
Unknown <qwinpin@gmail.com>
2017-12-05 02:09:56 +0400

    Refact, russian language support

--- just_fun.py
+++ just_fun.py
@@ -1,24 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Test module for overload requests
+"""
+import csv
+
 import bypass_api as ba
 from setting import set_parameters
-import csv
-#from pandas import read_csv
 
-#settings = set_parameters(querySearch = 'fuck', maxTweets = 300)
 res = []
 
-#lis = read_csv('C:\github\\twitter_crawler\mat2.csv', names = ['w', 'i'])
-lis2 = ['bitch','fuck','whore','suck', 'asshole', 'shit', 'cunt', 'putin']
-lis3 = ['asshole','shit','cunt', 'putin']
+lis2 = ['bitch', 'fuck', 'whore', 'suck', 'asshole', 'shit', 'cunt', 'putin']
+lis3 = ['любовь', 'cunt', 'putin']
 lis4 = ['bitcoin law', 'law', 'bitcoin']
+lll = ['уфа']
 
-lll = ['putin']
-for word in lis2:
-    time = ['2017-05-20', '2017-05-21']
-    settings = set_parameters(maxTweets = 0, since = time[0], until = time[1], querySearch = word)
+for word in lll:
+    time = ['2015-11-21', '2017-12-17']
+    settings = set_parameters(maxTweets=0, since=time[0], until=time[1], querySearch=word)
     data, err, cookies = ba.parse(settings)
     for tweets in data:
         res.append([tweets.text, tweets.screenname, tweets.created_at, tweets.geo])
-    print(res, err, len(res))
+    print(res, err)
     name = './data/' + time[0] + '_' + word + '.csv'
     with open('C:\\github\\twitter_crawler\\mattestr2.csv', 'a') as csv_file:
         csv_writer = csv.writer(csv_file)
