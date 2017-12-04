28cab5a059d6b00e207a249322a5f9c5b04e9657
FormMe <formme1995@yandex.ru>
2017-12-05 00:22:09 +0300

    bypass tasks sending recieving

--- consumer.py
+++ consumer.py
@@ -7,13 +7,6 @@ from multiprocessing import Pool, Queue
 import bypass_api as ba
 from setting import set_parameters, set_save_parameters
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-    host='localhost'))
-channel = connection.channel()
-
-channel.queue_declare(queue='task_queue', durable=True)
-print(' [*] Waiting for messages. To exit press CTRL+C')
-
 
 def main_worker(queue):
     pid = os.getpid()
@@ -21,34 +14,40 @@ def main_worker(queue):
     while True:
         #TODO не забыть поставить timeout или отправлять в очередь пустую таску
         task = queue.get(True)
-        query = task['query_param']
-        saving_params = task['save_param']
-        data, err, cook = ba.parse(query)
+        data, err, cook = ba.parse(task['query_param'])
         if err != 0:
             task['query_param']['cookies'] = cook
             queue.put(task)
 
         #TODO сохранение в бд на сервере
-        ba.to_csv(data, 'file_' + str(pid)+ '_', saving_params)
+        ba.to_csv(data, 'file_' + str(pid)+ '_', task['save_param'])
         print(os.getpid(), "crawled", len(data), "tweets")
 
 
-queue = Queue()
-pool = Pool(4, main_worker, (queue,))
 
 def callback(ch, method, properties, body):
     task = json.loads(body)
     print(" [x] Received " , task)
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
-    queue.put(task)
+    #queue.put(task)
 
-    pool.close()
-    pool.join()
+    # pool.close()
+    # pool.join()
 
 
-channel.basic_qos(prefetch_count=1)
-channel.basic_consume(callback,
-                      queue='task_queue')
+if __name__ == '__main__':
+    connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='localhost'))
+    channel = connection.channel()
 
-channel.start_consuming()
+    channel.queue_declare(queue='task_queue', durable=True)
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+
+    # queue = Queue()
+    # pool = Pool(4, main_worker, (queue,))
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(callback,
+                          queue='task_queue')
+
+    channel.start_consuming()
--- producer.py
+++ producer.py
@@ -4,28 +4,29 @@ import json
 from setting import set_parameters, set_save_parameters
 import numpy as np
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-    host='localhost'))
-channel = connection.channel()
+if __name__ == '__main__':
+    connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='localhost'))
+    channel = connection.channel()
 
-channel.queue_declare(queue='task_queue', durable=True)
+    channel.queue_declare(queue='task_queue', durable=True)
 
-for i in range(10, 30):
-    message = {
-        'query_param': set_parameters(maxTweets=300,
-                             since=('2017-01-' + str(i)),
-                             until=('2017-01-' + str(i + 1)),
-                             screen_name='xbox'),
+    for i in range(10, 30):
+        message = {
+            'query_param': set_parameters(maxTweets=300,
+                                 since=('2017-01-' + str(i)),
+                                 until=('2017-01-' + str(i + 1)),
+                                 screen_name='xbox'),
 
-        'save_param': set_save_parameters()
-    }
+            'save_param': set_save_parameters()
+        }
 
-    channel.basic_publish(exchange='',
-                          routing_key='task_queue',
-                          body=json.dumps(message),
-                          properties=pika.BasicProperties(
-                              delivery_mode=2,  # make message persistent
-                          ))
-    print(" [x] Sent %r" % (message,))
+        channel.basic_publish(exchange='',
+                              routing_key='task_queue',
+                              body=json.dumps(message),
+                              properties=pika.BasicProperties(
+                                  delivery_mode=2,  # make message persistent
+                              ))
+        print(" [x] Sent %r" % (message,))
 
-connection.close()
+    connection.close()
