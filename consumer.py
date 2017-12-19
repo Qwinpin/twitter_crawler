0c21da20a24cd810a3a37829122e21f1df07cdeb
FormMe <formme1995@yandex.ru>
2017-12-19 21:53:10 +0300

    mult workers forking

--- consumer.py
+++ consumer.py
@@ -1,54 +1,67 @@
 import pika
 import json
 import os
-import time
-from multiprocessing import Pool, Queue
+from multiprocessing import Pool, Queue, Process
 
 import bypass_api as ba
 
-def main_worker(queue):
-    pid = os.getpid()
-    print(pid, "working")
-    while True:
-        # TODO не забыть поставить timeout или отправлять в очередь пустую таску
-        task = queue.get(True)
-        data, err, cook = ba.parse(task['query_param'])
-        if err != 0:
+
+class Worker:
+    def __init__(self, host, port=5672, login='guest', password='guest'):
+        self.host = host
+        self.port = port
+        self.login = login
+        self.password = password
+
+    def callback(self, ch, method, properties, body):
+        task = json.loads(body)
+        print(" [x] Received ", task)
+
+        allData = []
+        while True:
+            data, err, cook = ba.parse(task['query_param'])
+            allData += data
+            if err == 0:
+                break
             task['query_param']['cookies'] = cook
-            queue.put(task)
 
         # TODO сохранение в бд на сервере
-        ba.to_csv(data, 'file_' + str(pid) + '_', task['save_param'])
-        print(os.getpid(), "crawled", len(data), "tweets")
+        ba.to_csv(allData, 'file_' + str(os.getpid()) + '_', task['save_param'])
+        print(os.getpid(), "crawled", len(allData), "tweets")
+
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+
+    def run(self):
+        credentials = pika.PlainCredentials(self.login, self.password)
+        parameters = pika.ConnectionParameters(self.host,
+                                               self.port,
+                                               '/',
+                                               credentials)
+        connection = pika.BlockingConnection(parameters)
+        channel = connection.channel()
+
+        channel.queue_declare(queue='task_queue', durable=True)
+        print(' [*] Waiting for messages. To exit press CTRL+C')
+
+        channel.basic_qos(prefetch_count=1)
+        channel.basic_consume(self.callback,
+                              queue='task_queue')
+
+        channel.start_consuming()
 
 
-def callback(ch, method, properties, body):
-    task = json.loads(body)
-    print(" [x] Received ", task)
-    ch.basic_ack(delivery_tag=method.delivery_tag)
-
-    queue.put(task)
+def run_worker(host):
+    w = Worker(host=host)
+    w.run()
 
 
 if __name__ == '__main__':
-    credentials = pika.PlainCredentials('guest', 'guest')
-    parameters = pika.ConnectionParameters('localhost',
-                                           5672,
-                                           '/',
-                                           credentials)
-    connection = pika.BlockingConnection(parameters)
-    channel = connection.channel()
+    N = 4
+    workers = []
+    for w in range(N):
+        w = Process(target=run_worker, args=('localhost',))
+        w.start()
+        workers.append(w)
 
-    channel.queue_declare(queue='task_queue', durable=True)
-    print(' [*] Waiting for messages. To exit press CTRL+C')
-
-    queue = Queue()
-    pool = Pool(4, main_worker, (queue,))
-    channel.basic_qos(prefetch_count=1)
-    channel.basic_consume(callback,
-                          queue='task_queue')
-
-    channel.start_consuming()
-
-    pool.close()
-    pool.join()
+    for w in workers:
+        w.join()
