664ed757e8b96df28efea05fa991a1c940f8f412
FormMe <formme1995@yandex.ru>
2017-12-19 23:08:09 +0300

    producer class

--- producer.py
+++ producer.py
@@ -1,32 +1,47 @@
 import pika
-import sys
 import json
+
 from setting import set_parameters, set_save_parameters
-import numpy as np
+
+
+class Producer:
+    def __init__(self, host, port=5672, login='guest', password='guest'):
+        self.login = login
+        self.password = password
+        self.host = host
+        self.port = port
+
+    def run(self):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(
+            host=self.host))
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue='task_queue', durable=True)
+
+    def stop(self):
+        self.connection.close()
+
+    def send_tasks(self, tasks):
+        for task in tasks:
+            self.channel \
+                .basic_publish(exchange='',
+                               routing_key='task_queue',
+                               body=task)
+            print(" [x] Sent %r" % (task,))
+
 
 if __name__ == '__main__':
-    connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host='localhost'))
-    channel = connection.channel()
-
-    channel.queue_declare(queue='task_queue', durable=True)
-
+    p = Producer('localhost')
+    p.run()
+    tasks = []
     for i in range(10, 30):
         message = {
             'query_param': set_parameters(maxTweets=300,
-                                 since=('2017-01-' + str(i)),
-                                 until=('2017-01-' + str(i + 1)),
-                                 screen_name='xbox'),
+                                          since=('2017-01-' + str(i)),
+                                          until=('2017-01-' + str(i + 1)),
+                                          screen_name='xbox'),
 
             'save_param': set_save_parameters()
         }
-
-        channel.basic_publish(exchange='',
-                              routing_key='task_queue',
-                              body=json.dumps(message),
-                              properties=pika.BasicProperties(
-                                  delivery_mode=2,  # make message persistent
-                              ))
-        print(" [x] Sent %r" % (message,))
-
-    connection.close()
+        tasks.append(json.dumps(message))
+    p.send_tasks(tasks)
+    p.stop()
