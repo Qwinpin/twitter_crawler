30c7ec6200875e252c3b0d56065e445f29d59a3b
Unknown <qwinpin@gmail.com>
2018-01-19 17:49:05 +0300

    New functionality
    Likes, retweets, reply

--- bypass_api.py
+++ bypass_api.py
@@ -56,8 +56,8 @@ def parse(parameters, save_settings, receiveBuffer=None, bufferLength=100, proxy
             tweet = Tweet(save_settings)
             usernameTweet = tweetPQ("span:first.username.u-dir b").text()
             txt = re.sub(r"\s+", " ", tweetPQ("p.js-tweet-text").text().replace('# ', '#').replace('@ ', '@'))
-            #retweets = int(tweetPQ("span.ProfileTweet-action--retweet span.ProfileTweet-actionCount").attr("data-tweet-stat-count").replace(",", ""))
-            #favorites = int(tweetPQ("span.ProfileTweet-action--favorite span.ProfileTweet-actionCount").attr("data-tweet-stat-count").replace(",", ""))
+            retweets = int(tweetPQ("span.ProfileTweet-action--retweet span.ProfileTweet-actionCount").attr("data-tweet-stat-count"))
+            favorites = int(tweetPQ("span.ProfileTweet-action--favorite span.ProfileTweet-actionCount").attr("data-tweet-stat-count"))
             try:
                 dateSec = int(tweetPQ("small.time span.js-short-timestamp").attr("data-time"))
             except:
@@ -70,6 +70,37 @@ def parse(parameters, save_settings, receiveBuffer=None, bufferLength=100, proxy
             if len(geoSpan) > 0:
                 geo = geoSpan.attr('title')
 
+        
+            likes_users = []
+            likes_url = 'https://twitter.com/i/activity/favorited_popup?id=' + str(ids)
+            likes_headers = parameters['headers']
+            likes_headers['Referer'] = str(ids)
+            likes_cookieJar = requests.cookies.RequestsCookieJar()
+            try:
+                likes_response = requests.get((likes_url), cookies=likes_cookieJar,
+                                    headers=likes_headers)
+            except:
+            else:
+                likes = PyQuery(likes_response.json()['htmlUsers'])('ol')
+                for i in likes[0]:
+                    likes_users.append({'id': PyQuery(i)('div.account').attr('data-user-id'), \
+                                        'screenname': PyQuery(i)('div.account').attr('data-name')})
+
+            retweet_users = []
+            retweet_url = 'https://twitter.com/i/activity/retweeted_popup?id=' + str(ids)
+            retweet_headers = parameters['headers']
+            retweet_headers['Referer'] = str(ids)
+            retweet_cookieJar = requests.cookies.RequestsCookieJar()
+            try:
+                retweet_response = requests.get((retweet_url), cookies=retweet_cookieJar,
+                                    headers=retweet_headers)
+            except:
+            else:
+                retweet = PyQuery(retweet_response.json()['htmlUsers'])('ol')
+                for i in retweet[0]:
+                    retweet_users.append({'id': PyQuery(i)('div.account').attr('data-user-id'), \
+                                        'screenname': PyQuery(i)('div.account').attr('data-name')})
+
             tweet.id_str = ids
             tweet.permalink = 'https://twitter.com' + permalink
             tweet.screenname = usernameTweet
@@ -78,8 +109,8 @@ def parse(parameters, save_settings, receiveBuffer=None, bufferLength=100, proxy
                 tweet.created_at = datetime.datetime.fromtimestamp(dateSec).strftime("%Y-%m-%d %H:%M:%S")
             else:
                 tweet.created_at = '1970-01-01'
-            #tweet.retweets = retweets
-            #tweet.favorites = favorites
+            tweet.retweets = retweets
+            tweet.favorites = favorites
             tweet.mentions = " ".join(re.compile('(@\\w*)').findall(tweet.text))
             tweet.hashtags = " ".join(re.compile('(#\\w*)').findall(tweet.text))
             tweet.geo = geo
