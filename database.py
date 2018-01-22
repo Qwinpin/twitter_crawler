9581de3c57c699a079558c8a58d8c4718dd26503
Unknown <qwinpin@gmail.com>
2018-01-22 16:53:18 +0300

    Save new features

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 import csv
 import sqlite3 as sql
+from tqdm import tqdm
 
 class DataBase:
     def save(self, tweets, settings):
@@ -26,11 +27,17 @@ class SQLite3(DataBase):
         self.cursor = self.db.cursor()
     
     def save(self, tweets, query):
-        for row in tweets:
-            self.cursor.execute('''INSERT INTO tweets(id_str, screenname, created_at, text, url, reply_to)
-                  VALUES(?,?,?,?,?,?)''', (row.id_str,row.screenname, row.created_at, \
-                  row.text, query['url'], row.reply_to#, row.likes_users, row.retweet_users
-                  #'---'.join('{}{}'.format(key, val) for key, val in row.likes_users.items()), \
-                  #'---'.join('{}{}'.format(key, val) for key, val in row.retweet_users.items()
-                  ))
+        for row in tqdm(tweets):
+            try:
+                self.cursor.execute('''INSERT INTO tweets(id_str, screenname, created_at, text, \
+                    url, reply_to, favorites, replys, retweets, likes_users, retweet_users)
+                    VALUES(?,?,?,?,?,?,?,?,?,?,?)''', (row.id_str,row.screenname, row.created_at, \
+                    row.text, query['url'], row.reply_to, row.favorites, row.reply, row.retweets, \
+                    ', '.join(['-'.join('{} : {}'.format(key, value) for key, value in d.items()) \
+                                for d in row.likes_users]), 
+                    ', '.join(['-'.join('{} : {}'.format(key, value) for key, value in d.items()) \
+                                for d in row.retweet_users]), 
+                ))
+            except sql.IntegrityError as e:
+                pass
         self.db.commit()
