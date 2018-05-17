6f2de520b42e2f8c0e400070b0c10ab60c14186f
Alex Malyutin <malutin.alex@gmail.com>
2018-05-17 14:48:18 +0300

    Update UserAgent instance.

--- crawler/task_creator.py
+++ crawler/task_creator.py
@@ -3,12 +3,19 @@ import json
 import itertools
 import datetime
 from fake_useragent import UserAgent
+from fake_useragent import FakeUserAgentError
+
+try:
+    ua = UserAgent()
+except FakeUserAgentError:
+    print("Can't create UserAgent!")
+    pass
 
 
 def get_headers():
     return {
         'Host': "twitter.com",
-        'User-Agent': UserAgent().random,
+        'User-Agent': ua.random,
         'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
         'Accept-Language': "en-US,en;q=0.5",
         'Connection': "keep-alive"
